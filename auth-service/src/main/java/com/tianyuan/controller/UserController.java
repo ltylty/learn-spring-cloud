048c11095784d7f399c382c947e5edefec7d9464
tianyuan <1137588860@qq.com>
2017-08-11 23:51:30 +0800

    add account ui

---
+++ auth-service/src/main/java/com/tianyuan/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.tianyuan.controller;
+
+import java.io.IOException;
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping(value = "/current")
+    public Principal getUser(Principal principal) {
+        return principal;
+    }
+    
+    @RequestMapping("/exit")
+    public void exit(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, null, null);
+        try {
+            response.sendRedirect(request.getHeader("referer"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
